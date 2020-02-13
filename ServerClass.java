3d2479e6c39a5953b271d91ed079bffaf9858361
Vladislav <vlad18742@gmail.com>
2020-02-13 11:12:30 +0300

    Added database for users' logins and passwords, made the server to work with that logins and passwords.

--- ServerClass.java
+++ ServerClass.java
@@ -2,28 +2,58 @@ package task7;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ServerClass {
-	private static final int SERVER_PORT = 4507;
-	private static ArrayList<String> logins = new ArrayList<String>(5); //буду использовать массивы, пока не сделаю
-	private static ArrayList<String> passwords = new ArrayList<String>(5); //запись/чтение файлов
+	private static final int SERVER_PORT = 4506;
+	private static ArrayList<String> logins = new ArrayList<String>(5);
+	private static ArrayList<String> passwords = new ArrayList<String>(5);
 	private static String message;
+	
+	private static void writeDatabase() throws IOException{ //функция записи записи в файл
+		FileWriter output_logins = new FileWriter("Logins.txt");
+		FileWriter output_passwords = new FileWriter("Passwords.txt");
+		for(int i=0; i<logins.size(); i++){
+			output_logins.write(logins.get(i)+'\n');
+			output_passwords.write(passwords.get(i)+'\n');
+		}
+		output_logins.close();
+		output_passwords.close();
+	}
+	
+	private static void readDatabase() throws IOException{ //функция чтения файла
+		FileReader input_logins = new FileReader("Logins.txt");
+		FileReader input_passwords = new FileReader("Passwords.txt");
+		Scanner scan_logins = new Scanner(input_logins);
+		Scanner scan_passwords = new Scanner(input_passwords);
+		while(scan_logins.hasNextLine()){
+			logins.add(scan_logins.nextLine());
+		}
+		while(scan_passwords.hasNextLine()){
+			passwords.add(scan_passwords.nextLine());
+		}
+		input_logins.close();
+		input_passwords.close();
+	}
 	public static void main(String[] args) throws IOException {
+		readDatabase();
 		final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-		logins.add("Vlad");
-		passwords.add("12345");
 		try{
 			while(true){
 				final Socket socket = serverSocket.accept();
 				try{
 					final DataInputStream in = new DataInputStream(socket.getInputStream());
-					final String work_type = in.readUTF();
+					final String work_type = in.readUTF(); //режим работы сервера на данной итерации
 					if(work_type.equals("CHECK_IN")){//режим проверки логина и пароля
 						final String login = in.readUTF();
 						final String password = in.readUTF();
@@ -33,6 +63,26 @@ public class ServerClass {
 								message = "true";
 								break;
 							}
+						}
+					}
+					else if(work_type.equals("NEW_USER")){ //режим регистрации новой уч.записи
+						final String login = in.readUTF();
+						final String password = in.readUTF();
+						boolean repeated = false;
+						for(int i=0; i<logins.size(); i++){
+							if(login.equals(logins.get(i))){
+								repeated = true;
+								break;
+							}
+						}
+						if(repeated == false){
+							logins.add(login);
+							passwords.add(password);
+							message = "created";
+							writeDatabase();
+						}
+						else{
+							message = "not_created";
 						}
 					}
 					final String port = in.readUTF();
